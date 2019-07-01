c14910ff0b984c53a765c3f2f155c8098bf86775
Ruslan Salikhov <ruslan.salihov@gmail.com>
2019-07-02 01:11:31 +0500

    optimized a bit

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
     let counter = 0;
     let checked = new Set();
     preferences.forEach( (value, i) => {
-        if (!checked.has(i))
-            if (value-1 !== i && preferences[preferences[value-1]-1]-1 === i ) {
+        if (!checked.has(i) && value - 1 > i)
+            if (preferences[preferences[value-1]-1]-1 === i ) {
                 counter++;
                 checked.add(value-1);
-                checked.add(preferences[value-1]-1);
             }
     });
     return counter;
