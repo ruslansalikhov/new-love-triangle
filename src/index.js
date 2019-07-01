17a7c84cdad5fd43ff3fa95dfb60b8bbb9e2da2c
Ruslan Salikhov <ruslan.salihov@gmail.com>
2019-07-02 00:59:43 +0500

    straightforward (naive) solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let counter = 0;
+    let checked = new Set();
+    preferences.forEach( (value, i) => {
+        if (!checked.has(i))
+            if (value-1 !== i && preferences[preferences[value-1]-1]-1 === i ) {
+                counter++;
+                checked.add(value-1);
+                checked.add(preferences[value-1]-1);
+            }
+    });
+    return counter;
 };
